2b44db03f311cdef89135476d4d129dd50b2397c
tushar-7932-singh <tusharsinghchauhan81@gmail.com>
2026-09-04 00:10:54 +0530

    Time: 5505 ms (88.60%) | Memory: 12.4 MB (57.81%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board, word):
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c, index):
+            if index == len(word):
+                return True
+
+            if (r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                board[r][c] != word[index]):
+                return False
+
+            # Mark cell as visited
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            found = (
+                dfs(r + 1, c, index + 1) or
+                dfs(r - 1, c, index + 1) or
+                dfs(r, c + 1, index + 1) or
+                dfs(r, c - 1, index + 1)
+            )
+
+            # Backtrack
+            board[r][c] = temp
+
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
